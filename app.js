a3ccd74d51ef2bd70041471a8a235a80955272a7
buyubaya <hihihi_caigikia@yahoo.com>
2019-01-21 22:22:15 -0800

    'HELLO'

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+// BODY PARSER
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// HEADERS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    next();
+});
 
 // ROUTES
 const projectsRoute = require('./routes/projects');
-
 app.use('/projects', projectsRoute);
 
 // ERRORS
